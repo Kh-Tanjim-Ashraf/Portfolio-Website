fe1f7c318b01d8fc5b58277c6c70bc926536f158
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-08-28 14:55:09 +0600

    Divide the `Skills` API endpoint to two separate endpoints (`Skills`, `SkillDetail`) to conveniently comply authentication permission for the `POST`, `PATCH` & `DELETE` method; Make the `return Response()` more specific to the block-scope of `serializer.is_valid()` rather than the `post()` & `patch()` methods of `SkillDetail` class

--- kh-tanjim-ashraf-portfolio-backend/api/urls.py
+++ kh-tanjim-ashraf-portfolio-backend/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from account.views import Profile
-from portfolio.views import Skills
+from portfolio.views import Skills, SkillDetail
 
 urlpatterns = [
     path('auth/', include('account.urls')),
     path('profile/', view=Profile.as_view()),   # TODO: Restructure the API url structure by moving the `Profile` model to a separate app called `ProfileDashboard`
     path('skills/', view=Skills.as_view()),
-    path('skills/<id>/', view=Skills.as_view()),
+    path('skills/<id>/', view=SkillDetail.as_view()),
 ]
--- kh-tanjim-ashraf-portfolio-backend/portfolio/views.py
+++ kh-tanjim-ashraf-portfolio-backend/portfolio/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render
-from .models import Skill as SkillModel
+from .models import Skill, Experience
 from rest_framework.views import APIView
-from .serializers import SkillSerializer
+from .serializers import SkillSerializer, ExperienceSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from .filters import SkillFilter
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 
+# Skill
 class Skills(APIView):
 
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            self.permission_classes = [AllowAny]
+
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAuthenticated]
+        
+        return [permission() for permission in self.permission_classes]
+
     def get(self, request):
-        queryset = SkillModel.objects.all()
+        queryset = Skill.objects.all()
 
         filterset = SkillFilter(data=request.query_params, queryset=queryset)
 
@@ -20,33 +31,38 @@ class Skills(APIView):
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+            serializer = SkillSerializer(data=request.data)
+    
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+    
+                return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+class SkillDetail(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, id):
-        queryset = get_object_or_404(SkillModel, pk=id)
+        queryset = get_object_or_404(Skill, pk=id)
 
         serializer = SkillSerializer(instance=queryset)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        serializer = SkillSerializer(data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-
-        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-
     def patch(self, request, id):
-        queryset = get_object_or_404(SkillModel, pk=id)
+        queryset = get_object_or_404(Skill, pk=id)
 
         serializer = SkillSerializer(instance=queryset, data=request.data, partial=True)
 
         if serializer.is_valid(raise_exception=True):
             serializer.save()
 
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, id):
-        queryset = get_object_or_404(SkillModel, pk=id)
+        queryset = get_object_or_404(Skill, pk=id)
 
         queryset.delete()
 
