051976b1e4b5d9605954bf2c7832cd1cc7313e0f
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-09-04 04:38:08 +0600

    (Feat) Implemented the filter for `?tag=` query-param in `Posts` APIView class utilizing `django_filter`; Both single tag & comma-separated tags are handled

--- kh-tanjim-ashraf-portfolio-backend/blog/filters.py
+++ kh-tanjim-ashraf-portfolio-backend/blog/filters.py
@@ -8,6 +8,7 @@ class PostFilter(django_filters.FilterSet):
 
     status = django_filters.CharFilter(method='filter_by_status', label='Filter by post status (Draft, published, all)')
     category = django_filters.CharFilter(method='filter_by_category', label='Filter by category slug')
+    tag = django_filters.CharFilter(method='filter_by_tags', label='Filter by tag slug; Multiple tags accepted (comma-separated)')
 
     class Meta:
         model = Post
@@ -60,3 +61,23 @@ class PostFilter(django_filters.FilterSet):
 
         # The `name` is the name of the query-param
         return self.filter_by_user_permission(queryset, 'category__slug', value)
+
+    def filter_by_tags(self, queryset, name, value):
+        '''
+        Both single tag & comma-separated tags are handled
+        '''
+        
+        # Check if multiple comma-separated tag-values are sent from the API client
+        if len(value.split(',')) > 1:
+            value_list = [val.strip() for val in value.split(',') if val.strip()]
+            
+            # If the list is empty
+            if not value_list:
+                return queryset
+
+            # Slugify each value of the list
+            value = [slugify(val) for val in value_list]
+            return self.filter_by_user_permission(queryset, 'tag__slug__in', value).distinct()  # Remove duplicates
+        else:
+            value = slugify(value)
+            return self.filter_by_user_permission(queryset, 'tag__slug', value).distinct()
