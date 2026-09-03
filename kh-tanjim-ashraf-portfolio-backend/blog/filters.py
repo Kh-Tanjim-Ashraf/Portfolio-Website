a948394f3eff1e964566e6ad0d2508de55f78d09
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-09-04 00:51:22 +0600

    (Refactor) Updated the code quality of `filter_by_status` method

--- kh-tanjim-ashraf-portfolio-backend/blog/filters.py
+++ kh-tanjim-ashraf-portfolio-backend/blog/filters.py
@@ -36,22 +36,19 @@ class PostFilter(django_filters.FilterSet):
 
     # `name=status` & `value=<value of the query-param>`
     def filter_by_status(self, queryset, name, value):
-        user = self.request.user
         value = value.lower()
         
-        owner_privileged_status_query = ['draft','all']
+        admin_privileged_status_query = ['draft','all']
 
         # For admin
-        if user.is_authenticated \
-            and user.is_superuser \
-            and value in owner_privileged_status_query:
+        if self.isAdmin() and value in admin_privileged_status_query:
 
             # For draft posts
-            if value == owner_privileged_status_query[0]:
+            if value == admin_privileged_status_query[0]:
                 return queryset.filter(status=value)
                 
             # For all posts
-            if value == owner_privileged_status_query[1]:
+            if value == admin_privileged_status_query[1]:
                 return queryset
         
         # For anonymous users: If uses the `?status=` query-param with whether "draft/all" query-value, but will only get the "Published" posts
