a57d4eaeb59fd162374e28dbee3ca1869e761a8a
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-09-16 19:14:39 +0600

    (Deployment fix) Updated the `build_vercel.sh` shell-script to create a virtual environment adhering to Python isolated environment standards

--- kh-tanjim-ashraf-portfolio-backend/build_vercel.sh
+++ kh-tanjim-ashraf-portfolio-backend/build_vercel.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
 # Install the dependencies
 pip install -r requirements.txt
+
+pip install --upgrade pip
 
 # Let Django generate the static files (CSS, javascript, images & fonts)
 python manage.py collectstatic --noinput
